0eb8a0e236c76bedffd9f7ace869f95371cad39e
Rassoul Diouf <raidiouf@my.uri.edu>
2017-07-26 16:09:17 -0400

    Add files via upload

--- liblsl-Python/examples/fNIR_pullData.py
+++ liblsl-Python/examples/fNIR_pullData.py
@@ -8,6 +8,6 @@ stream_inlet = StreamInlet(fNIR_streams[0])
 
 while True:
     sample, timestamp = stream_inlet.pull_sample()
-    print(timestamp, sample)
+    print((timestamp, sample)+"\n")
 
 print("...Fin.")
