5d928413a0dd9424b85181360c18a3ca87e68a8c
Rassoul Diouf <raidiouf@my.uri.edu>
2017-07-26 16:07:18 -0400

    Add files via upload

---
+++ liblsl-Python/examples/fNIR_pullData.py
@@ -0,0 +1,13 @@
+from pylsl import StreamInlet, resolve_stream
+
+print('Looking for (fNIR) stream...')
+
+fNIR_streams = resolve_stream('type', 'NIRS')
+
+stream_inlet = StreamInlet(fNIR_streams[0])
+
+while True:
+    sample, timestamp = stream_inlet.pull_sample()
+    print(timestamp, sample)
+
+print("...Fin.")
