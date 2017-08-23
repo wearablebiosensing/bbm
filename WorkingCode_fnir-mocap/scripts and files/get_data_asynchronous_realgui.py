5eea9b23f76445683e3f882fbb712472e4ff6aa9
raidiouf <raidiouf@gmail.com>
2017-08-23 17:16:56 -0400

    checkbox

--- WorkingCode_fnir-mocap/scripts and files/get_data_asynchronous_realgui.py
+++ WorkingCode_fnir-mocap/scripts and files/get_data_asynchronous_realgui.py
@@ -15,6 +15,7 @@ global hips, chest, r_lower_arm, r_hand, l_upper_leg, l_lower_leg, l_foot
 global r_upper_leg, r_lower_leg, r_foot
 
 
+
 root=Tk()
 
 # text box GUI
@@ -58,6 +59,160 @@ class SimpleTable(tk.Frame):
 ##if __name__ == "__main__":
 ##    app = ExampleApp()
 
+def PopUp():
+    
+    popup=Tk()
+    popup.geometry('250x400')
+    popup.grid()
+    popup.title("Sensor Quantity")
+    label = Label(popup, text='Display Sensors')
+    label.grid()
+    
+    global l_shoulder,r_shoulder,l_upper_arm, l_lower_arm, l_hand, r_upper_arm, head
+    global hips, chest, r_lower_arm, r_hand, l_upper_leg, l_lower_leg, l_foot
+    global r_upper_leg, r_lower_leg, r_foot
+    
+    global all_list, sensor_list, chosen_sensor_list
+    l_shoulder = r_shoulder = l_upper_arm = l_lower_arm = l_hand = r_upper_arm = head= False
+    hips = chest = r_lower_arm = r_hand = l_upper_leg = l_lower_leg = l_foot = False
+    r_upper_leg = r_lower_leg = r_foot = False
+    
+    var1=IntVar()
+    var2=IntVar()
+    var3=IntVar()
+    var4=IntVar()
+    var5=IntVar()
+    var6=IntVar()
+    var7=IntVar()
+    var8=IntVar()
+    var9=IntVar()
+    var10=IntVar()
+    var11=IntVar()
+    var12=IntVar()
+    var13=IntVar()
+    var14=IntVar()
+    var15=IntVar()
+    var16=IntVar()
+    var17=IntVar()
+    var18=IntVar()
+    
+    c18=tk.Checkbutton(popup,text="All", variable=var18, \
+                    onvalue=1,offvalue=0, height=5)
+    if (var18.get()):
+        var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14,var15,var16,var17=1
+    c18.grid(row=10,column=1)
+    
+    c1=tk.Checkbutton(popup,text="l_shoulder",variable=var1, \
+                   onvalue=1,offvalue=0)
+
+
+    if (var1.get()):
+        l_shoulder=True
+    c1.grid(row=1,column=0)
+
+    c2=tk.Checkbutton(popup,text="r_shoulder",variable=var2, \
+               onvalue=1,offvalue=0)
+    if (var2.get()):
+        r_shoulder=True
+    c2.grid(row=1,column=1)
+
+    c3=tk.Checkbutton(popup,text="l_upper_arm",variable=var3, \
+                   onvalue=1,offvalue=0)
+    if (var3.get()):
+        l_upper_arm=True
+    c3.grid(row=2,column=0)
+
+    c4=tk.Checkbutton(popup,text="l_lower_arm",variable=var4, \
+                   onvalue=1,offvalue=0)
+    if (var4.get()):
+        l_lower_arm=True
+    c4.grid(row=3,column=0)
+
+    c5=tk.Checkbutton(popup,text="l_hand",variable=var5, \
+               onvalue=1,offvalue=0)
+    if (var5.get()):
+        l_hand=True
+    c5.grid(row=4,column=0)
+
+    c6=tk.Checkbutton(popup,text="r_upper_arm",variable=var6, \
+                   onvalue=1,offvalue=0)
+    if (var6.get()):
+        r_upper_arm=True
+    c6.grid(row=2,column=1)
+
+    c7=tk.Checkbutton(popup,text="head",variable=var7, \
+                onvalue=1,offvalue=0)
+    if (var7.get()):
+        head=True
+    c7.grid(row=1,column=2)
+
+    c8=tk.Checkbutton(popup,text="hips",variable=var8, \
+               onvalue=1,offvalue=0)
+    if (var8.get()):
+        hips=True
+    c8.grid(row=3,column=2)
+
+    c9=tk.Checkbutton(popup,text="chest",variable=var9, \
+                   onvalue=1,offvalue=0)
+    if (var9.get()):
+        chest=True
+    c9.grid(row=2,column=2)
+
+    c10=tk.Checkbutton(popup,text="r_lower_arm",variable=var10, \
+                    onvalue=1,offvalue=0)
+    if (var10.get()):
+        r_lower_arm=True
+    c10.grid(row=3,column=1)
+
+    c11=tk.Checkbutton(popup,text="r_hand",variable=var11, \
+                onvalue=1,offvalue=0)
+    if (var11.get()):
+        r_hand=True
+    c11.grid(row=4,column=1)
+
+    c12=tk.Checkbutton(popup,text="l_upper_leg",variable=var12, \
+                    onvalue=1,offvalue=0)
+    if (var12.get()):
+        l_upper_leg=True
+    c12.grid(row=6,column=0)
+
+    c13=tk.Checkbutton(popup,text="l_lower_leg",variable=var13, \
+                onvalue=1,offvalue=0)
+    if (var13.get()):
+        l_lower_leg=True
+    c13.grid(row=7,column=0)
+
+    c14=tk.Checkbutton(popup,text="l_foot",variable=var14, \
+                onvalue=1,offvalue=0)
+    if (var14.get()):
+        l_foot=True
+    c14.grid(row=8,column=0)
+
+    c15=tk.Checkbutton(popup,text="r_upper_leg",variable=var15, \
+                    onvalue=1,offvalue=0)
+    if var15.get():
+        r_upper_leg=True
+    c15.grid(row=6,column=1)
+
+    c16=tk.Checkbutton(popup,text="r_lower_leg",variable=var16, \
+                    onvalue=1,offvalue=0)
+    if var16.get():
+        r_lower_leg=True
+    c16.grid(row=7,column=1)
+
+    c17=tk.Checkbutton(popup,text="r_foot",variable=var17, \
+                onvalue=1,offvalue=0)
+    if var17.get():
+        r_foot=True
+    c17.grid(row=8,column=1)
+    
+
+    btn1=Button(popup, text='Apply', command=sensorID_string_commit )
+    btn1.grid(row=11,column=2)
+
+    #btn1.destroy()
+    
+    popup.mainloop()
 
 def sensorID_string_commit():
     global l_shoulder, r_shoulder, l_upper_arm, l_lower_arm, l_hand, r_upper_arm, head
@@ -65,9 +220,15 @@ def sensorID_string_commit():
     global r_upper_leg, r_lower_leg, r_foot
     global all_list, sensor_list, chosen_sensor_list
     global sensorID_string_to_sensorName
-    
+
+    device_list = ts_api.getComPorts()
+
+    all_list = []
+    sensor_list = []
+    chosen_sensor_list = []
+    chosen_sensor_list = []
     sensorID_string_to_sensorName = {
-    "'"+str("<YEI3Space WL:1200085A>")+"'": l_shoulder,
+        "'"+str("<YEI3Space WL:1200085A>")+"'": l_shoulder,
         "'"+str("<YEI3Space WL:1200085B>")+"'": r_shoulder,
         "'"+str("<YEI3Space WL:1200085C>")+"'": l_upper_arm,
         "'"+str("<YEI3Space WL:1200085D>")+"'": l_lower_arm,
@@ -85,6 +246,9 @@ def sensorID_string_commit():
         "'"+str("<YEI3Space WL:12000866>")+"'": r_lower_leg,
         "'"+str("<YEI3Space WL:12000867>")+"'": r_foot
     }
+
+    print(sensorID_string_to_sensorName)
+    
     for device_port in device_list:
         com_port, friendly_name, device_type = device_port
         device = None
@@ -100,6 +264,8 @@ def sensorID_string_commit():
             device = ts_api.TSDLSensor(com_port=com_port)
         elif device_type == "BT":
             device = ts_api.TSBTSensor(com_port=com_port)
+##        tex.insert(tk.END, device_type)
+##        tex.see(tk.END)
         
         if device is not None:
             all_list.append(device)
@@ -110,9 +276,33 @@ def sensorID_string_commit():
                     sens = device[i]
                     if sens is not None:
                         sensor_list.append(sens)
+##                        tex.insert(tk.END, str(sens))
+##                        tex.see(tk.END)
                         if sensorID_string_to_sensorName.get("'"+str(sens)+"'") is True:
                             chosen_sensor_list.append(sens)
+    cmt=str(chosen_sensor_list)
+    tex.insert(tk.END,cmt)
+    tex.see(tk.END)
 
+
+    with open('mocapData.txt', 'w') as f:
+        f.write(str(chosen_sensor_list)+'\n\n')
+    f.close()
+
+
+    ## The YEI 3-Space Python API has a global broadcaster called global_broadcaster
+    ## which is an instance of Broadcaster
+    ts_api.global_broadcaster.setStreamingTiming(   interval=128041,
+                                                    duration=4294967295,
+                                                    delay=200000,
+                                                    delay_offset=0,
+                                                    filter=chosen_sensor_list)
+    ts_api.global_broadcaster.setStreamingSlots(
+                                            slot0='getRawAccelerometerData',
+                                            slot1='getBatteryPercentRemaining',
+                                            filter=chosen_sensor_list)
+    ts_api.global_broadcaster.startStreaming(filter=chosen_sensor_list)
+    
 
 def connect():
     print("its just a test")
@@ -127,12 +317,12 @@ def connect():
     tex.insert(tk.END,string)
     tex.see(tk.END)
 
-    device_list = ts_api.getComPorts()
+##    device_list = ts_api.getComPorts()
 
     global all_list, sensor_list, chosen_sensor_list
-    all_list = []
-    sensor_list = []
-    chosen_sensor_list = []
+##    all_list = []
+##    sensor_list = []
+##    chosen_sensor_list = []
     
     global l_shoulder, r_shoulder, l_upper_arm, l_lower_arm, l_hand, r_upper_arm, head
     global hips, chest, r_lower_arm, r_hand, l_upper_leg, l_lower_leg, l_foot
@@ -190,23 +380,6 @@ def connect():
 #                        if sensorID_string_to_sensorName.get("'"+str(sens)+"'") is True:
 #                            chosen_sensor_list.append(sens)
 
-    with open('mocapData.txt', 'w') as f:
-        f.write(str(chosen_sensor_list)+'\n\n')
-    f.close()
-
-
-    ## The YEI 3-Space Python API has a global broadcaster called global_broadcaster
-    ## which is an instance of Broadcaster
-    ts_api.global_broadcaster.setStreamingTiming(   interval=128041,
-                                                    duration=4294967295,
-                                                    delay=200000,
-                                                    delay_offset=0,
-                                                    filter=chosen_sensor_list)
-    ts_api.global_broadcaster.setStreamingSlots(
-                                            slot0='getRawAccelerometerData',
-                                            slot1='getBatteryPercentRemaining',
-                                            filter=chosen_sensor_list)
-    ts_api.global_broadcaster.startStreaming(filter=chosen_sensor_list)
 
     print("======MoCap connected success===============")
     connSuccess=("======MoCap connected success===============\n")
@@ -294,157 +467,6 @@ def reset():
     tex.see(tk.END)
 
     return reset
-
-def PopUp():
-    popup=Tk()
-    popup.geometry('250x400')
-    popup.grid()
-    popup.title("Sensor Quantity")
-    label = Label(popup, text='Display Sensors')
-    label.grid()
-    
-    global l_shoulder,r_shoulder,l_upper_arm, l_lower_arm, l_hand, r_upper_arm, head
-    global hips, chest, r_lower_arm, r_hand, l_upper_leg, l_lower_leg, l_foot
-    global r_upper_leg, r_lower_leg, r_foot
-    
-    global all_list, sensor_list, chosen_sensor_list
-    
-    var1=IntVar()
-    var2=IntVar()
-    var3=IntVar()
-    var4=IntVar()
-    var5=IntVar()
-    var6=IntVar()
-    var7=IntVar()
-    var8=IntVar()
-    var9=IntVar()
-    var10=IntVar()
-    var11=IntVar()
-    var12=IntVar()
-    var13=IntVar()
-    var14=IntVar()
-    var15=IntVar()
-    var16=IntVar()
-    var17=IntVar()
-    var18=IntVar()
-    
-    
-    c1=Checkbutton(popup,text="l_shoulder",variable=var1, \
-                   onvalue=1,offvalue=0)
-    if (var1==1):
-        l_shoulder=True
-    c1.grid(row=1,column=0)
-
-    c2=Checkbutton(popup,text="r_shoulder",variable=var2, \
-               onvalue=1,offvalue=0)
-    if (var2==1):
-        r_shoulder=True
-    c2.grid(row=1,column=1)
-
-    c3=Checkbutton(popup,text="l_upper_arm",variable=var3, \
-                   onvalue=1,offvalue=0)
-    if (var3==1):
-        l_upper_arm=True
-    c3.grid(row=2,column=0)
-
-    c4=Checkbutton(popup,text="l_lower_arm",variable=var4, \
-                   onvalue=1,offvalue=0)
-    if (var4==1):
-        l_lower_arm=True
-    c4.grid(row=3,column=0)
-
-    c5=Checkbutton(popup,text="l_hand",variable=var5, \
-               onvalue=1,offvalue=0)
-    if (var5==1):
-        l_hand=True
-    c5.grid(row=4,column=0)
-
-    c6=Checkbutton(popup,text="r_upper_arm",variable=var6, \
-                   onvalue=1,offvalue=0)
-    if (var6==1):
-        r_upper_arm=True
-    c6.grid(row=2,column=1)
-
-    c7=Checkbutton(popup,text="head",variable=var7, \
-                onvalue=1,offvalue=0)
-    if (var7==1):
-        head=True
-    c7.grid(row=1,column=2)
-
-    c8=Checkbutton(popup,text="hips",variable=var8, \
-               onvalue=1,offvalue=0)
-    if (var8==1):
-        hips=True
-    c8.grid(row=3,column=2)
-
-    c9=Checkbutton(popup,text="chest",variable=var9, \
-                   onvalue=1,offvalue=0)
-    if (var9==1):
-        chest=True
-    c9.grid(row=2,column=2)
-
-    c10=Checkbutton(popup,text="r_lower_arm",variable=var10, \
-                    onvalue=1,offvalue=0)
-    if (var10==1):
-        r_lower_arm=True
-    c10.grid(row=3,column=1)
-
-    c11=Checkbutton(popup,text="r_hand",variable=var11, \
-                onvalue=1,offvalue=0)
-    if (var11==1):
-        r_hand=True
-    c11.grid(row=4,column=1)
-
-    c12=Checkbutton(popup,text="l_upper_leg",variable=var12, \
-                    onvalue=1,offvalue=0)
-    if (var12==1):
-        l_upper_leg=True
-    c12.grid(row=6,column=0)
-
-    c13=Checkbutton(popup,text="l_lower_leg",variable=var13, \
-                onvalue=1,offvalue=0)
-    if (var13==1):
-        l_lower_leg=True
-    c13.grid(row=7,column=0)
-
-    c14=Checkbutton(popup,text="l_foot",variable=var14, \
-                onvalue=1,offvalue=0)
-    if (var14==1):
-        l_foot=True
-    c14.grid(row=8,column=0)
-
-    c15=Checkbutton(popup,text="r_upper_leg",variable=var15, \
-                    onvalue=1,offvalue=0)
-    if (var15==1):
-        r_upper_leg=True
-    c15.grid(row=6,column=1)
-
-    c16=Checkbutton(popup,text="r_lower_leg",variable=var16, \
-                    onvalue=1,offvalue=0)
-    if (var16==1):
-        r_lower_leg=True
-    c16.grid(row=7,column=1)
-
-    c17=Checkbutton(popup,text="r_foot",variable=var17, \
-                onvalue=1,offvalue=0)
-    if (var17==1):
-        r_foot=True
-    c17.grid(row=8,column=1)
-    c18=Checkbutton(popup,text="All", variable=var18, \
-                    onvalue=1,offvalue=0, height=5)
-    if (var18==1):
-        var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14,var15,var16,var17=True
-    elif(var18==0):
-        var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14,var15,var16,var17=0
-    c18.grid(row=10,column=1)
-
-
-    btn1=Button(popup, text='Apply', command=sensorID_string_commit )
-    btn1.grid(row=11,column=2)
-
-    #btn1.destroy()
-    
-    popup.mainloop()
 
 
 #Menu
